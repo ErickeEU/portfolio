c5713736e21c338b3469db5e35d291444554624c
Ueric Azevedo <uerickalmeida@gmail.com>
2024-07-28 16:19:14 -0300

    estilizando a sidebar

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/ErickeEU.png" />
+
+export default Avatar
--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/avatar'
 import Title from '../../components/title'
+import Paragrafo from '../../components/paragraph'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ErickeEU.png" alt="" />
-    <Title fontSize={20}>Ericke</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Ericke</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ErickeEU
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
